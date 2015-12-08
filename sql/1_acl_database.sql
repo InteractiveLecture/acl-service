d01a011d955e8cd468f97be4614101fe7b32b8ad
Rene Richter <Richterrettich@gmail.com>
2015-12-08 09:22:17 +0100

    Initial commit

---
+++ sql/1_acl_database.sql
@@ -0,0 +1,33 @@
+
+
+create user aclapp;
+
+
+create database acl owner aclapp;
+
+\c acl
+
+drop table if exists object_identities cascade;
+drop table if exists acl_entries cascade;
+
+create table object_identities (
+  object_class varchar(500) not null,
+  id UUID PRIMARY KEY,
+  parent_object UUID,
+  owner_sid UUID not null,
+  constraint fk_acl_obj_parent foreign key(parent_object)references object_identities(id)
+);
+
+create table acl_entries (
+  object_id UUID not null references object_identities(id),
+  sid UUID not null,
+  create_permission boolean not null,
+  read_permission boolean not null,
+  update_permission boolean not null,
+  delete_permission boolean not null,
+  PRIMARY KEY (object_id,sid)
+);
+
+
+
+
