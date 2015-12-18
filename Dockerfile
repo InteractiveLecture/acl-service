05f334a956ea8ac62704b3f07e03f7c2c839b29e
Rene Richter <Richterrettich@gmail.com>
2015-12-18 22:56:55 +0100

    Progerss

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,4 @@
-FROM scratch
-#ADD ca-certificates.crt /etc/ssl/certs/
+FROM alpine:3.1 
 ADD out/main /main
-ADD tmp /tmp
-#ADD mime.types /etc/mime.types
 cmd ["/main"]
 EXPOSE 8080 
--- build.sh
+++ build.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 GO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o out/main .
-docker build -t interactive-lecture/lecture-service .
-
-cd sql
-./build.sh
-cd ..
+docker build -t openservice/acl-service .
